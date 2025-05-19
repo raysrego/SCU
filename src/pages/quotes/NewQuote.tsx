d0b2c95e2ec4459ae4c1e7f1e5bcabf9756f0125
raysrego <rayannyrego@gmail.com>
2025-05-18 23:35:03 -0300

    NewQuote.tsx

---
+++ src/pages/quotes/NewQuote.tsx
@@ -0,0 +1,11 @@
+// src/pages/quotes/NewQuote.tsx
+const NewQuote = () => {
+  return (
+    <div>
+      <h1>New Quote Page</h1>
+      {/* Sua lógica aqui */}
+    </div>
+  );
+};
+
+export default NewQuote;
